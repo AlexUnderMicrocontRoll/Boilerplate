573ef1041e659de1ae9c041d425a668ddedbf4d5
schaaf <alexander.schaaf@dfki.uni-kl.de>
2016-04-13 16:58:39 +0200

    cleaned komments weg etc

--- CNC/Interface/requests.js
+++ CNC/Interface/requests.js
@@ -1,47 +1,45 @@
 /**
  * Created by alexanderschaaf on 28.03.16.
  */
+var initialize = function() {
+    var xhr = new XMLHttpRequest();
 
-var xhr = new XMLHttpRequest();
-
-xhr.open("GET", "http://botnet.artificial.engineering:8080/api/Status",true);
-xhr.send();
+    xhr.open("GET", "http://botnet.artificial.engineering:8080/api/Status", true);
+    xhr.send();
 
 
-xhr.onload = function() {
-	var text = xhr.response;
-	var data = null;
-	
-	try {
-		data = JSON.parse(xhr.response);
-	} catch(e) {
-		console.error(e);
-	}
-	
-	
-    if (this.status == 200) {
-        console.log(data[0].ip);
-    }
-    
-    
-    var table = document.querySelector('#status-overview tbody');
-    
-    var row = null;
-   
-    
-    for (var i = 0; i < data.length; i++) {
-		row = table.insertRow(i);
-		
-		row.innerHTML = "<td>" + data[i].id + "</td><td>" + data[i].ip + "</td><td>"+ data[i].task + "</td><td>"+ data[i].workload + "</td>"
-	}
-    
-    
-   
-    
-    
-    
-    
-    
+    xhr.onload = function () {
+
+        var data = null;
+
+        try {
+            data = JSON.parse(xhr.response);
+        } catch (e) {
+            console.error(e);
+        }
+
+        if (this.status == 200) {
+            console.log(data[0].ip);
+        }
+
+        var table = document.querySelector('#status-overview tbody');
+        var row = null;
+        for (var i = 0; i < data.length; i++) {
+            row = table.insertRow(i);
+
+            row.innerHTML = "<td>" + data[i].id + "</td><td>" + data[i].ip + "</td><td>" + data[i].task + "</td><td>" + data[i].workload + "</td>"
+        }
+    };
 };
+
+    
+    
+   
+    
+    
+    
+    
+    
+
 
 
