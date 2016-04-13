806eaf6c8f05169250be167788f0508060979f55
karatayev <1610370@stud.hs-mannheim.de>
2016-04-13 16:31:17 +0200

    request.js gets bots from artificial.engineering

--- CNC/Interface/requests.js
+++ CNC/Interface/requests.js
@@ -2,15 +2,46 @@
  * Created by alexanderschaaf on 28.03.16.
  */
 
-console.log("wtf");
-var oReq = new XMLHttpRequest();
-oReq.open("GET", "http://botnet.artificial.engineering:8080/api/Status",true);
-oReq.responseType = "json";
-oReq.send();
-oReq.onload = function() {
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "http://botnet.artificial.engineering:8080/api/Status",true);
+xhr.send();
+
+
+xhr.onload = function() {
+	var text = xhr.response;
+	var data = null;
+	
+	try {
+		data = JSON.parse(xhr.response);
+	} catch(e) {
+		console.error(e);
+	}
+	
+	
     if (this.status == 200) {
-        console.log(this.response);
+        console.log(data[0].ip);
     }
+    
+    
+    var table = document.querySelector('#status-overview tbody');
+    
+    var row = null;
+   
+    
+    for (var i = 0; i < data.length; i++) {
+		row = table.insertRow(i);
+		
+		row.innerHTML = "<td>" + data[i].id + "</td><td>" + data[i].ip + "</td><td>"+ data[i].task + "</td><td>"+ data[i].workload + "</td>"
+	}
+    
+    
+   
+    
+    
+    
+    
+    
 };
 
 
