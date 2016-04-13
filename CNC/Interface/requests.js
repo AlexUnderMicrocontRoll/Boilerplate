ae4a5259378208248d91a397a3f884d8cccab5c0
schaaf <alexander.schaaf@dfki.uni-kl.de>
2016-04-13 15:32:09 +0200

    requests.js modified

--- CNC/Interface/requests.js
+++ CNC/Interface/requests.js
@@ -5,29 +5,12 @@
 console.log("wtf");
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "http://botnet.artificial.engineering:8080/api/Status",true);
-oReq.responseType = "text";
+oReq.responseType = "json";
 oReq.send();
-oReq.onload = function(e) {
+oReq.onload = function() {
     if (this.status == 200) {
-        var bb = new Text();
+        console.log(this.response);
     }
 };
 
-
-
-function addRow() {
-    var table = document.getElementById("status-overview");
-    var tbody = document.createElement("TBODY");
-    var trow = document.createElement("TR");
-    var tcolumn = document.createElement("TD");
-
-    tbody.setAttribute("id","tbody");
-    trow.setAttribute("id","trow");
-    tcolumn.setAttribute("id","tcolumn");
-
-    trow.appendChild(tcolumn);
-    tbody.appendChild(trow);
-    table.appendChild(tbody);
-
-}
 
