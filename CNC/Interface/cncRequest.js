0dd31b2d950df055a2a886c03b8fa33ae2304f5a
karatayev <1610370@stud.hs-mannheim.de>
2016-04-27 21:48:39 +0200

    sorting made persistent (Der Tragödie zweiter Teil)

--- CNC/Interface/cncRequest.js
+++ CNC/Interface/cncRequest.js
@@ -6,23 +6,33 @@
 
 // cnc server URI
 var cncServer = "http://botnet.artificial.engineering:8080/api/Status";
-var stop_reload; // TODO rename
-
+var periodicReload;
 
 var botnetData;
-var botnetDataSortProp = "id";
-var botnetDataSortOrder = "asc"; // asc, desc
 
 
-var initializePageReload = function() {
-	console.log("periodic page reload started");
-    stop_reload= setInterval(function(){cncServerRequest();}, 100000);
+/**
+ * Enables periodic data requests from cnc server.
+ */
+var enablePeriodicCNCRequest = function() {
+    periodicReload = setInterval(function(){cncServerRequest();}, 10000);
+    console.log("periodic cnc request: started");
 };
 
-var Reloadstop = function() {
-	clearTimeout(stop_reload);
+
+/**
+ * Disables periodic data requests from cnc server.
+ * 
+ */
+var disablePeriodicCNCRequest = function() {
+	clearTimeout(periodicReload);
+	console.log("periodic cnc request: stopped");
 };
 
+
+/**
+ * Requests botnet status data from cnc server.
+ */
 var cncServerRequest = function() {
     var xhr = new XMLHttpRequest();
 
@@ -43,7 +53,6 @@ var cncServerRequest = function() {
 	// onload event handler
     xhr.onload = function () {
 		
-
 		// try to parse response to json
         try {
 			// TODO xhr.response.setCharacterEncoding("UTF-8");
@@ -52,20 +61,27 @@ var cncServerRequest = function() {
             console.error(e);
         }
         
+        sortBotnetData();
 		updateStatusTable();
         console.log("cnc server bot list reloaded");
     };
 };
 
+/**
+ * Clears a html table.
+ */
 function clearTable(table) {
     var rows = table.rows;
-    var i = rows.length-1;
-    for( var k=i; k >=0; k--){
+    var i = rows.length - 1;
+    
+    for( var k = i; k >= 0; k--){
         table.deleteRow(k);
     }
 }
 
-
+/**
+ * Updates status table with botnet status data.
+ */
 function updateStatusTable() {
 	var statusTable = document.querySelector('#status-overview-results');
     var row = null;
@@ -77,16 +93,15 @@ function updateStatusTable() {
 
 	// fill table with new table rows
     for (var i = 0; i < botnetData.length; i++) {
-		console.log(botnetData[i].ip);
         row = statusTable.insertRow(i);
         row.innerHTML = "<td>" + botnetData[i].id + "</td><td>" + botnetData[i].ip + "</td><td>" + botnetData[i].workload + "</td>";
             
         if(botnetData[i].task === 0) {
 			// inactive, button says start
-			row.innerHTML += "<td><button type=\"button\" id=\"data[i].id\" onclick=Postrequest_Task(this.id)>Start</button></td>";
+			row.innerHTML += "<td><button type=\"button\">Start</button></td>";
 		} else {
 			// running, button says stop
-			row.innerHTML += "<td><button type=\"button\" id=\"data[i].id\" onclick=Postrequest_Task(this.id)>Stop</button></td>";
+			row.innerHTML += "<td><button type=\"button\">Stop</button></td>";
 		}
 	}
 }
@@ -96,8 +111,5 @@ function updateStatusTable() {
     
     
     
-    
-    
-
 
 
--- CNC/Interface/sort.js
+++ CNC/Interface/sort.js
@@ -1,45 +1,82 @@
 /**
- * Created by AlouScha on 27.04.16.
+ * Responsible for sorting of botnetData.
+ * 
+ * @author Alex, Sai, Tobi
  */
 
-var sort = function (key) {
+var botnetDataSortProp = "id";	// id/ip/workload/task
+var botnetDataSortOrder = "asc"; // ascending, descending sort order
 
-    botnetDataSortProp = key;
-
-    if (botnetDataSortOrder === "asc") {
-        botnetData.sort(byPropertyAsc(botnetDataSortProp));
+/**
+ * Switches botnet data sort order between ascending and descending.
+ */
+var swapBotnetDataSortOrder = function() {
+	if(botnetDataSortOrder === "asc") {
         botnetDataSortOrder = "desc";
-        console.log("status table sort: id descending");
-    }else if (botnetDataSortOrder === "desc") {
-        botnetData.sort(byPropertyDesc(botnetDataSortProp));
-        botnetDataSortOrder = "asc";
-        console.log("status table sort: id ascending");
     }
+    else if (botnetDataSortOrder === "desc") {
+        botnetDataSortOrder = "asc";
+    }
+    else {
+		console.log("warning: status table sort order should be either asc or desc");
+	}
+};
 
+
+/**
+ * Sets sorting property and refreshes the status table.
+ */
+var setSorting = function(property) {
+	botnetDataSortProp = property;
+	
+	swapBotnetDataSortOrder();
+	sortBotnetData();
     updateStatusTable();
 };
 
-var byPropertyAsc = function (prop) {
 
-    return function (a, b) {
-        if (typeof a[prop] === 'string' || a[prop] instanceof String) {
-            //für strings localCompare
-            return a[prop].localeCompare(b[prop]);
-        } else {
-            //sonst für nummern
-            return a[prop] - b[prop];
-        }
+/**
+ * Sorts botnet data appropriately.
+ */
+var sortBotnetData = function() {
+    if(botnetDataSortOrder === "asc") {
+        botnetData.sort(byPropertyAsc(botnetDataSortProp));
+    }
+    else if (botnetDataSortOrder === "desc") {
+        botnetData.sort(byPropertyDesc(botnetDataSortProp));
+    }
+};
+
+
+/**
+ * Ascending compare function.
+ * @prop prop property of the object used for comparison
+ */
+var byPropertyAsc = function(prop) {
+    return function(a,b) {
+            if (typeof a[prop] === 'string' || a[prop] instanceof String){
+                //für strings localCompare
+                return a[prop].localeCompare(b[prop]);
+            }else{
+                //sonst für nummern
+                return a[prop] - b[prop];
+            }
     };
 };
 
-var byPropertyDesc = function (prop) {
+
+/**
+ * Descending compare function.
+ * @prop prop property of the object used for comparison
+ */
+var byPropertyDesc = function(prop) {
     return function (a, b) {
-        if (typeof a[prop] === 'string' || a[prop] instanceof String) {
-            //für strings localCompare
-            return b[prop].localeCompare(a[prop]);
-        } else {
-            //sonst für nummern
-            return b[prop] - a[prop];
-        }
+            if (typeof a[prop] === 'string' || a[prop] instanceof String){
+                //für strings localCompare
+                return b[prop].localeCompare(a[prop]);
+            }else{
+                //sonst für nummern
+                return b[prop] - a[prop];
+            }
     };
 };
