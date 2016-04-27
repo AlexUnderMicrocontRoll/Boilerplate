a06fba059ce2a11e9aa27d39af8b6b5224a3f889
schaaf <alexander.schaaf@dfki.uni-kl.de>
2016-04-27 09:28:54 +0200

    moved sort stuff into sort.js

--- CNC/Interface/cncRequest.js
+++ CNC/Interface/cncRequest.js
@@ -23,47 +23,6 @@ var Reloadstop = function() {
 	clearTimeout(stop_reload);
 };
 
-
-var sort = function(key){
-    botnetDataSortProp = key;
-
-    if(botnetDataSortOrder === "asc") {
-        botnetData.sort(byPropertyAsc(botnetDataSortProp));
-        botnetDataSortOrder = "desc";
-        console.log("status table sort: id descending");
-    }
-    else if (botnetDataSortOrder === "desc") {
-        botnetData.sort(byPropertyDesc(botnetDataSortProp));
-        botnetDataSortOrder = "asc";
-        console.log("status table sort: id ascending");
-    }
-    updateStatusTable();
-};
-
-var byPropertyAsc = function(prop) {
-    return function(a,b) {
-            if (typeof a[prop] === 'string' || a[prop] instanceof String){
-                //für strings localCompare
-                return a[prop].localeCompare(b[prop]);
-            }else{
-                //sonst für nummern
-                return a[prop] - b[prop];
-            }
-    };
-};
-
-var byPropertyDesc = function(prop) {
-    return function (a, b) {
-            if (typeof a[prop] === 'string' || a[prop] instanceof String){
-                //für strings localCompare
-                return b[prop].localeCompare(a[prop]);
-            }else{
-                //sonst für nummern
-                return b[prop] - a[prop];
-            }
-    };
-};
-
 var cncServerRequest = function() {
     var xhr = new XMLHttpRequest();
 
