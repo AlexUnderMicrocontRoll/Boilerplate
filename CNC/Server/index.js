1725592487a4982481ec9457662f7b5d0c69a19b
SumDim <saikam@hotmail.de>
2016-06-13 11:46:32 +0200

    Update index.js

--- CNC/Server/index.js
+++ CNC/Server/index.js
@@ -78,7 +78,6 @@ var respond_NOTOK_Like = (res) => {
  */
 var updateStatus = (reqBody)=> {
 		var isFound = false;
-//anderen namen verwenden eventuell nicht item da item nach tasks sucht?
 //Muss noch getestet werden
 		status.forEach((item) => {
         if (item.id == reqBody.id) {
